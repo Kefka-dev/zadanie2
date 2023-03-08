0b396a5cc28a97872da162ba828315ff654d436a
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-03-08 18:48:23 +0100

    priebeh normalneho kola spraveny
    len samotny pohyb a vypis

--- main.c
+++ main.c
@@ -103,7 +103,46 @@ int main() {
         printf("%d", trat[i]);
     }
     printf("\n");
+
+    
     // 8. Vypis pretekov.
+    //koniec dostihov = vestky kone v ciely alrbo vsetky kone eliminovane
+    int kone[K], PosBefore[K], PosAfter[K];
+    //koneDead =1 vsetky kone umrely, =0 aspon jeden zije
+    int koneDead, roll, round = 1;
+    for (int i = 0; i < K; i++)
+    {
+        kone[i] = 1;       //vsetky kone na zaciatku ziju
+        PosBefore[i] = -1; //pociatocna pozicia vsetkych konov je -1
+        PosAfter[i] = -1;
+    }
+    
+    // kde i je cislo kona 
+    for (int i = 0; i < K; i++)
+    {
+        roll = rnd(1, 6);
+        PosAfter[i] = PosAfter[i] + roll;
+        //check ci nadzigali
+        if (trat[PosAfter[i]] == 1)
+        {
+            PosAfter[i] = PosAfter[i] + 1;
+            if (trat[PosAfter[i]] == 1)
+            {
+                printf("%3d %3d %3d %3d %3d F\n", round, i+1, PosBefore[i], roll, PosAfter[i]);
+            }
+            else
+            {
+                printf("%3d %3d %3d %3d %3d N\n", round, i+1, PosBefore[i], roll, PosAfter[i] + 1);
+            }
+        }
+        else
+        {
+            printf("%3d %3d %3d %3d %3d \n", round, i+1, PosBefore[i], roll, PosAfter[i]);
+        }
+        PosBefore[i] = PosAfter[i];
+    }
+    
+    
     // 9. Vypis poradia koni v cieli.
 
     return 0;
